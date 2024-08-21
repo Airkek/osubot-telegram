0677ad4cd285277e456a221a55f6dc371cc3ffe3
Keijia <ke1jia@vk.com>
2024-08-21 15:48:48 +0300

    revert last commit

--- src/pp/bancho/Calculator.ts
+++ src/pp/bancho/Calculator.ts
@@ -70,7 +70,6 @@ class BanchoPP implements ICalc {
         const fcAttrs = new rosu.Performance({ 
             mods: this.mods.flags,
             clockRate: this.speedMultiplier,
-            accuracy: score.accuracy(),
             n300: score.counts[300] + score.counts.miss,
             n100: score.counts[100],
             n50: score.counts[50],
@@ -86,7 +85,6 @@ class BanchoPP implements ICalc {
             nGeki: score.counts.geki,
             nKatu: score.counts.katu,
             misses: score.counts.miss,
-            accuracy: score.accuracy(),
             combo: score.combo,
         }).calculate(rmap);
 
