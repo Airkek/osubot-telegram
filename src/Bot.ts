d28b32f395c389ad48db10c0a42542418166eb69
Anton Babichev <ke1jia@vk.com>
2025-02-18 17:57:21 +0300

    fix

--- src/Bot.ts
+++ src/Bot.ts
@@ -248,7 +248,7 @@ export class Bot {
         this.database.run("CREATE TABLE IF NOT EXISTS covers (id INTEGER, attachment TEXT)");
         this.database.run("CREATE TABLE IF NOT EXISTS photos (url TEXT, attachment TEXT)");
         this.database.run("CREATE TABLE IF NOT EXISTS errors (code TEXT, info TEXT, error TEXT)");
-        this.database.run(`CREATE TABLE IF NOT EXISTS users_to_chat (user INTEGER, chat INTEGER)`);
+        this.database.run(`CREATE TABLE IF NOT EXISTS users_to_chat ("user" INTEGER, chat INTEGER)`);
     }
 
     async start() {
