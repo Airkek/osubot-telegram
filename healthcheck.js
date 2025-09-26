6695e5c70f472dce710d29e0fdd16d2acdd6d90e
keijia <ke1jia@vk.com>
2025-09-27 00:58:19 +0300

    fix healthcheck

---
+++ healthcheck.js
@@ -0,0 +1,25 @@
+﻿/* eslint-disable */
+const http = require("http");
+
+const options = {
+    host: "localhost",
+    port: Number(process.env.PORT),
+    path: "/health",
+    timeout: 10000,
+};
+
+const req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    if (res.statusCode === 200) {
+        process.exit(0);
+    } else {
+        process.exit(1);
+    }
+});
+
+req.on("error", (e) => {
+    console.error(`ERROR: ${e.message}`);
+    process.exit(1);
+});
+
+req.end();
